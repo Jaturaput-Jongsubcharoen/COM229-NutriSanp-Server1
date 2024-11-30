4ed4c1467fbf0f08905622578b4a4dfda45ab4aa
Sukhmanpreet715 <sehmbisukhman@gmail.com>
2024-11-30 14:20:51 -0500

    test1
    testing

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const jwt = require("jsonwebtoken");
 const path = require("path");
 
 const app = express();
-const PORT = 5000;
+const PORT = 5000; // abc
 
 // MongoDB connection
 const MONGO_URI = "mongodb+srv://JaturaputJongsubcharoen:mac0840747314@comp229.evxxr.mongodb.net";
