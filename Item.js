f5cb91bd7ba406295aa9296c5f8e1a4ba930c3cb
Jaturaput-Jongsubcharoen <macker.jong@gmail.com>
2024-12-06 12:28:14 -0500

    Jaturaput-Backend24

--- Item.js
+++ Item.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const itemSchema = new mongoose.Schema({
-    name: String,
-    description: String
-})
-
+    Name: { type: String, required: true }, 
+    Calories: { type: String, required: true }, 
+    Protein: { type: String, required: true }, 
+    Fat: { type: String, required: true }, 
+    Carbohydrates: { type: String, required: true },
+});
 
 const itemModel = mongoose.model("Item", itemSchema);
 module.exports = itemModel;
